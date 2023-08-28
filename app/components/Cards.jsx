92f8e748d991be9b4a9747d06103992ff2baa008
Alex Ghezea <alexghezea98@yahoo.com>
2023-08-28 22:33:53 +0300

    Update Cards.jsx

--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -1,10 +1,7 @@
-"use client"
-import React from 'react'
-import {CodeBracketIcon} from "@heroicons/react/24/solid";
-import Image from 'next/image'
-
-
-
+"use client";
+import React from "react";
+import { CodeBracketIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 export const Cards = () => {
   return (
@@ -19,123 +16,136 @@ export const Cards = () => {
             These are some of the app that i've build.
           </p>
         </div>
-<div className= "lg:h-screen flex flex-wrap row-auto justify-center mx-auto">
+        <div className="lg:h-screen flex flex-wrap row-auto justify-center mx-auto">
+          <div class=" text-gray-900 p-2">
+            <div>
+              <img
+                src="/project1.jpg"
+                width={500}
+                height={500}
+                alt="project img"
+                className="relative rounded-lg shadow-lg mx-auto"
+              />
 
- <div class=" text-gray-900 p-2">
-          <div>
-            <Image
-              src={'/project1.jpg'}
-              width={500}
-              height={500}
-              alt='project img'
-              className='relative rounded-lg shadow-lg mx-auto'
-            />
+              <div class="relative px-4 -mt-16  ">
+                <div class="bg-white p-6 rounded-lg shadow-lg">
+                  <div class="flex items-baseline">
+                    <div class="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider text-center">
+                      REACT &bull; CSS &bull; JS
+                    </div>
+                  </div>
 
-            <div class="relative px-4 -mt-16  ">
-              <div class="bg-white p-6 rounded-lg shadow-lg">
-                <div class="flex items-baseline">
-                  <div class="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider text-center">
-                  REACT &bull; CSS &bull; JS
+                  <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">
+                    Star Wars
+                  </h4>
+
+                  <div class="mt-2">
+                    <a
+                      href="https://star-wars-mini.netlify.app"
+                      target="_blank"
+                    >
+                      <button class="text-purple-700 text-2xl font-semibold p-2">
+                        Live
+                      </button>
+                    </a>
+                    <a
+                      href="https://github.com/DoNKsar/star-wars-wannabe"
+                      target="_blank"
+                    >
+                      <button class="text-2xl text-gray-600 p-2">Code</button>
+                    </a>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
 
-                <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">
-                  Star Wars 
-                </h4>
+          <div class=" text-gray-900 p-2">
+            <div>
+              <img
+                src="/Project2.jpg"
+                width={500}
+                height={500}
+                alt="project img"
+                className="relative rounded-lg shadow-lg mx-auto"
+              />
 
-                <div class="mt-2">
-                  <a href="">
-                    <button class="text-purple-700 text-2xl font-semibold p-2">
-                      Live
-                    </button>
-                  </a>
-                  <a href="">
-                    <button class="text-2xl text-gray-600 p-2">Code</button>
-                  </a>
+              <div class="relative px-4 -mt-16  ">
+                <div class="bg-white p-6 rounded-lg shadow-lg">
+                  <div class="flex items-baseline">
+                    <div class="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider text-center">
+                      HTML/BOOTSRAP &bull; CSS &bull; JS/JQuery
+                    </div>
+                  </div>
+
+                  <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">
+                    Anime Project
+                  </h4>
+
+                  <div class="mt-2">
+                    <a
+                      href="https://anime-test-project.netlify.app"
+                      target="_blank"
+                    >
+                      <button class="text-purple-700 text-2xl font-semibold p-2">
+                        Live
+                      </button>
+                    </a>
+                    <a
+                      href="https://github.com/DoNKsar/Anime-Site-try-hard"
+                      target="_blank"
+                    >
+                      <button class="text-2xl text-gray-600 p-2">Code</button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=" text-gray-900 p-2">
+            <div>
+              <img
+                src="/project3.jpg"
+                width={500}
+                height={500}
+                alt="project img"
+                className="relative rounded-lg shadow-lg mx-auto"
+              />
+
+              <div class="relative px-4 -mt-16  ">
+                <div class="bg-white p-6 rounded-lg shadow-lg">
+                  <div class="flex items-baseline">
+                    <div class="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider text-center">
+                      Next js &bull; Tailwind
+                    </div>
+                  </div>
+
+                  <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">
+                    IT Portfolio
+                  </h4>
+
+                  <div class="mt-2">
+                    <a
+                      href="https://wannabe-portfolio-it.vercel.app/ "
+                      target="_blank"
+                    >
+                      <button class="text-purple-700 text-2xl font-semibold p-2">
+                        Live
+                      </button>
+                    </a>
+                    <a
+                      href="https://github.com/DoNKsar/Wannabe-Portfolio-IT"
+                      target="_blank"
+                    >
+                      <button class="text-2xl text-gray-600 p-2">Code</button>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
-        <div class=" text-gray-900 p-2">
-          <div>
-            <Image
-              src={'/Project2.jpg'}
-              width={500}
-              height={500}
-              alt='project img'
-              className='relative rounded-lg shadow-lg mx-auto'
-            />
-
-            <div class="relative px-4 -mt-16  ">
-              <div class="bg-white p-6 rounded-lg shadow-lg">
-                <div class="flex items-baseline">
-                  <div class="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider text-center">
-                    HTML/BOOTSRAP &bull; CSS &bull; JS/JQuery
-                  </div>
-                </div>
-
-                <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">
-                  Anime Project
-                </h4>
-
-                <div class="mt-2">
-                  <a href="">
-                    <button class="text-purple-700 text-2xl font-semibold p-2">
-                      Live
-                    </button>
-                  </a>
-                  <a href="">
-                    <button class="text-2xl text-gray-600 p-2">Code</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class=" text-gray-900 p-2">
-          <div>
-            <Image
-              src={'/project3.jpg'}
-              width={500}
-              height={500}
-              alt='project img'
-              className='relative rounded-lg shadow-lg mx-auto'
-            />
-
-            <div class="relative px-4 -mt-16  ">
-              <div class="bg-white p-6 rounded-lg shadow-lg">
-                <div class="flex items-baseline">
-                  <div class="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider text-center">
-                    Next js &bull; Tailwind
-                  </div>
-                </div>
-
-                <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">
-                  IT Portfolio
-                </h4>
-
-                <div class="mt-2">
-                  <a href="">
-                    <button class="text-purple-700 text-2xl font-semibold p-2">
-                      Live
-                    </button>
-                  </a>
-                  <a href="">
-                    <button class="text-2xl text-gray-600 p-2">Code</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-
-</div>
-       
       </div>
     </section>
   );
-}
+};
